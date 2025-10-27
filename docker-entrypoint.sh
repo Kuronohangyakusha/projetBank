3eb3a35c2915ed22d1f3eb7bf60631a10f2d104d
Your Name <your.email@example.com>
2025-10-27 20:29:54 +0000

    correction

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Wait for database to be ready
 echo "Waiting for database to be ready..."
-until pg_isready -h dpg-d3t2brvgi27c73e2aedg-a.oregon-postgres.render.com -p 5432 -U bankmanager_gk5o_user; do
+until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USERNAME; do
   echo "Database is unavailable - sleeping"
   sleep 2
 done
